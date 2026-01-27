51d5801d9181567a973c1522f62f51121abbc4e2
Cristian <cristianabreuberredoo@gmail.com>
2026-01-27 16:08:59 -0300

    Arrumando importações

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+// IMPORTE USANDO await import() DINÂMICO
+const honoConfig = async () => {
+  const { default: build } = await import('@hono/vite-build/cloudflare-pages')
+  const { default: devServer } = await import('@hono/vite-dev-server')
+  const { default: adapter } = await import('@hono/vite-dev-server/cloudflare')
+
+  return defineConfig({
+    plugins: [
+      build(),
+      devServer({
+        adapter,
+        entry: 'src/index.tsx'
+      })
+    ],
+    server: {
+      port: 3000,
+      host: true
+    }
+  })
+}
+
+export default honoConfig()
